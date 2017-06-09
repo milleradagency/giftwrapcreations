f1a9d59cb011d570c7c0e5790a688b28334aba4a
William Pansky <williampansky@gmail.com>
2017-06-09 09:58:46 -0500

    v0.4.0
    ready to show client for approval, edits, etc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+// document.ready
+
 // HOMEPAGE SLIDER
 jQuery(function($) {
 
@@ -16,10 +18,13 @@ jQuery(function($) {
   let next1         = $("#hpt-next1");
   let back2         = $("#hpt-back2");
   let next2         = $("#hpt-next2");
+  let back3         = $("#hpt-back3");
+  let next3         = $("#hpt-next3");
 
   let active        = "homepage-tabs-active";
   let one           = $("#homepage-tabs-one");
   let two           = $("#homepage-tabs-two");
+  let three         = $("#homepage-tabs-three");
 
   // prevent default
   $("#homepage-tabs a").click( function( e ) {
@@ -32,9 +37,20 @@ jQuery(function($) {
   $("#homepage-tabs-one [class^=homepage-tabs-text]").velocity( slideUpIn, { stagger: 300, drag: true, } );
   $("#homepage-tabs-one .homepage-tabs-img").velocity( fadeIn );
 
+
   // controls 1
-  back1.click( kill1_for2 );
+  back1.click( kill1_for3 );
   next1.click( kill1_for2 );
+
+  function kill1_for3() {
+    // These multiple animate calls are queued to run one after the other
+    $("#homepage-tabs-one [class^=homepage-tabs-text]").velocity( slideLeftOut, { stagger: 100, drag: false, } );
+    $("#homepage-tabs-one .homepage-tabs-img").velocity( fadeOut );
+    $("#homepage-tabs-one").velocity( fadeOut );
+    $("#homepage-tabs-three").velocity( fadeIn );
+    $("#homepage-tabs-three [class^=homepage-tabs-text]").velocity( slideRightIn, { delay: 300, stagger: 300, drag: true, } );
+    $("#homepage-tabs-three .homepage-tabs-img").velocity( fadeIn );
+  }
 
   function kill1_for2() {
     // These multiple animate calls are queued to run one after the other
@@ -46,15 +62,51 @@ jQuery(function($) {
     $("#homepage-tabs-two .homepage-tabs-img").velocity( fadeIn );
   }
 
+
   // controls 2
   back2.click( kill2_for1 );
-  next2.click( kill2_for1 );
+  next2.click( kill2_for3 );
 
   function kill2_for1() {
     // These multiple animate calls are queued to run one after the other
     $("#homepage-tabs-two [class^=homepage-tabs-text]").velocity( slideLeftOut, { stagger: 100, drag: false, } );
     $("#homepage-tabs-two .homepage-tabs-img").velocity( fadeOut );
     $("#homepage-tabs-two").velocity( fadeOut );
+    $("#homepage-tabs-one").velocity( fadeIn );
+    $("#homepage-tabs-one [class^=homepage-tabs-text]").velocity( slideRightIn, { delay: 300, stagger: 300, drag: true, } );
+    $("#homepage-tabs-one .homepage-tabs-img").velocity( fadeIn );
+  }
+
+  function kill2_for3() {
+    // These multiple animate calls are queued to run one after the other
+    $("#homepage-tabs-two [class^=homepage-tabs-text]").velocity( slideLeftOut, { stagger: 100, drag: false, } );
+    $("#homepage-tabs-two .homepage-tabs-img").velocity( fadeOut );
+    $("#homepage-tabs-two").velocity( fadeOut );
+    $("#homepage-tabs-three").velocity( fadeIn );
+    $("#homepage-tabs-three [class^=homepage-tabs-text]").velocity( slideRightIn, { delay: 300, stagger: 300, drag: true, } );
+    $("#homepage-tabs-three .homepage-tabs-img").velocity( fadeIn );
+  }
+
+
+  // controls 3
+  back3.click( kill3_for2 );
+  next3.click( kill3_for1 );
+
+  function kill3_for2() {
+    // These multiple animate calls are queued to run one after the other
+    $("#homepage-tabs-three [class^=homepage-tabs-text]").velocity( slideLeftOut, { stagger: 100, drag: false, } );
+    $("#homepage-tabs-three .homepage-tabs-img").velocity( fadeOut );
+    $("#homepage-tabs-three").velocity( fadeOut );
+    $("#homepage-tabs-two").velocity( fadeIn );
+    $("#homepage-tabs-two [class^=homepage-tabs-text]").velocity( slideRightIn, { delay: 300, stagger: 300, drag: true, } );
+    $("#homepage-tabs-two .homepage-tabs-img").velocity( fadeIn );
+  }
+
+  function kill3_for1() {
+    // These multiple animate calls are queued to run one after the other
+    $("#homepage-tabs-three [class^=homepage-tabs-text]").velocity( slideLeftOut, { stagger: 100, drag: false, } );
+    $("#homepage-tabs-three .homepage-tabs-img").velocity( fadeOut );
+    $("#homepage-tabs-three").velocity( fadeOut );
     $("#homepage-tabs-one").velocity( fadeIn );
     $("#homepage-tabs-one [class^=homepage-tabs-text]").velocity( slideRightIn, { delay: 300, stagger: 300, drag: true, } );
     $("#homepage-tabs-one .homepage-tabs-img").velocity( fadeIn );
